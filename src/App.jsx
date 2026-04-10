1a52707ae9c36449f40efec6a92e623836949594
jakemsr <jakemsr@yahoo.com>
2026-04-09 23:51:37 -0700

    - let the Login page know if use is already logged in - add id's to form elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import "./index.css"
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import Home from "./pages/Home/Home"
@@ -12,11 +12,16 @@ const App = () => {
 
   const navigate = useNavigate();
 
+  // the Login page needs to know if user is already logged in
+  const loggedIn = useRef(false);
+
   useEffect(() => {
     onAuthStateChanged(auth, async (user) => {
       if (user) {
+        loggedIn.current = true;
         navigate("/");
       } else {
+        loggedIn.current = false;
         navigate("/login");
       }
     })
@@ -27,7 +32,7 @@ const App = () => {
       <ToastContainer theme="dark" />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login loggedIn={loggedIn.current} />} />
         <Route path="/player/:id" element={<Player />} />
       </Routes>
     </div>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import "./Login.css"
 import logo from "../../assets/logo.png"
 import { login, signup } from "../../firebase"
 import netflix_spinner from "../../assets/netflix_spinner.gif"
 
-const Login = () => {
+const Login = (loggedIn) => {
 
   const [signState, setSignState] = useState("Sign In");
 
@@ -12,6 +13,19 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+
+  // If we're already logged in, we shouldn't be here. Go forward
+  // one page.
+  // We can end up here when we go back from the Player because we
+  // assume we need to go back 2 pages, which is usually, but not 
+  // always right, and it's very difficult to determine if we need 
+  // to go back one or two because of iFrame content we don't control.
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (loggedIn) {
+      navigate(1);
+    }
+  }, [])
 
   const userAuth = async (event) => {
     event.preventDefault();
@@ -25,39 +39,39 @@ const Login = () => {
   }
 
   return (
-    loading?<div className="login-spinner">
+    loading ? <div className="login-spinner">
       <img src={netflix_spinner} alt="" />
-    </div>:
-    <div className="login">
-      <img src={logo} className='login-logo' alt="" />
-      <div className="login-form">
-        <h1>{signState}</h1>
-        <form>
-          {signState === "Sign Up" ?
-            <input type="text" placeholder="Your name"
-            value={name} onChange={(e) => {setName(e.target.value)}} /> :
-            <></>}
-          <input type="email" placeholder='Email'
-          value={email} onChange={(e) => {setEmail(e.target.value)}} />
-          <input type="password" placeholder='Password'
-          value={password} onChange={(e) => {setPassword(e.target.value)}} />
-          <button onClick={userAuth} type="submit">{signState}</button>
-          <div className="form-help">
-            <div className="remember">
-              <input type="checkbox" />
-              <label htmlFor="">Remember Me</label>
+    </div> :
+      <div className="login">
+        <img src={logo} className='login-logo' alt="" />
+        <div className="login-form">
+          <h1>{signState}</h1>
+          <form>
+            {signState === "Sign Up" ?
+              <input type="text" placeholder="Your name" id="name"
+                value={name} onChange={(e) => { setName(e.target.value) }} /> :
+              <></>}
+            <input type="email" placeholder='Email' id="email"
+              value={email} onChange={(e) => { setEmail(e.target.value) }} />
+            <input type="password" placeholder='Password' id="password"
+              value={password} onChange={(e) => { setPassword(e.target.value) }} />
+            <button onClick={userAuth} type="submit">{signState}</button>
+            <div className="form-help">
+              <div className="remember">
+                <input type="checkbox" id="remember" />
+                <label htmlFor="remember">Remember Me</label>
+              </div>
+              <p>Need Help?</p>
             </div>
-            <p>Need Help?</p>
+          </form>
+          <div className="form-switch">
+            {signState === "Sign In" ?
+              <p>New to Netflix? <span onClick={() => setSignState("Sign Up")}>Sign Up Now</span></p> :
+              <p>Already have account? <span onClick={() => setSignState("Sign In")}>Sign In Now</span></p>
+            }
           </div>
-        </form>
-        <div className="form-switch">
-          { signState === "Sign In" ?
-            <p>New to Netflix? <span onClick={() => setSignState("Sign Up")}>Sign Up Now</span></p> :
-            <p>Already have account? <span onClick={() => setSignState("Sign In")}>Sign In Now</span></p>
-          }
         </div>
       </div>
-    </div>
   )
 }
 
